ac2205f08654ba6bc8ed5e9ba310a283a1dfdce7
NickTheWhale <loehrketurkey7@gmail.com>
2024-02-06 22:41:39 +0000

    init

---
+++ build/CMakeFiles/hello_dev.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_dev.dir/main.c.o"
+  "hello_dev"
+  "hello_dev.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello_dev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
